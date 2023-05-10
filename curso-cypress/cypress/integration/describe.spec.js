da7cf6421571a6986d923c73097011909a281d7e
Fran <francieliceleghim2013@gmail.com>
2023-05-10 10:39:52 -0300

    Localizar e interagir com elemento

---
+++ curso-cypress/cypress/integration/describe.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+it.skip('A external test...', () => {
+
+})
+
+describe('Shold group tests...', () => {
+  describe('Should group more specific tests...', () => {
+    it('A specific test...', () => {
+
+    })
+
+  })
+
+  describe('Should group more specific tests...', () => {
+    it('A specific test...', () => {
+      
+    })
+
+  })
+
+
+  it.skip('A internal test...', () => {
+
+  })
+})
