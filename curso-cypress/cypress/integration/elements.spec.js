2027baae56d35a28c808a9eb981a1ac4b495b3d6
Fran <francieliceleghim2013@gmail.com>
2023-05-15 15:58:07 -0300

    Aplicando checkbox

--- curso-cypress/cypress/integration/elements.spec.js
+++ curso-cypress/cypress/integration/elements.spec.js
@@ -43,6 +43,27 @@ describe( 'Work with basic elements', () => {
       .clear()
       .type('Erro{selectall}acerto', { delay: 100})
       .should('have.value', 'acerto')
+
+  
+    cy.get('#formSexoFem')
+      .click()
+      .should('be.checked')
+        
+    cy.get('#formSexoMasc').should('not.be.checked')
+    
+    cy.get("[name='formSexo']").should('have.length', 2)
+
   })
+
+  it.only('Checkbox', () => {
+    cy.get('#formComidaPizza')
+      .click()
+      .should('be.checked')
+
+    cy.get('[name=formComidaFavorita]').click({multiple: true})
+    cy.get('#formComidaPizza').should('not.be.checked')
+    cy.get('#formComidaVegetariana').should('be.checked')
+  })
+  
 })
 
